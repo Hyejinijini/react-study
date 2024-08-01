ff22c6937e51bb05e7e0a4371e3b2de7539dd991
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-02 05:48:28 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hello from "./components/Hello.js";
+import Welcome from "./components/Welcome.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import World from "./World.js";
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello</p>
+      <World />
+      <World />
+    </div>
+  );
+};
+
+export default Hello;
