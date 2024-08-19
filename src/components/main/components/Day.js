c6bfd3afaf6423e6402607fd5a2a813608b46932
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-19 19:10:23 +0900

    update: Day.js 수정
    useParams 훅을 import 해서 사용

--- src/components/main/components/Day.js
+++ src/components/main/components/Day.js
@@ -1,12 +1,17 @@
+import { useParams } from "react-router-dom";
 import dummy from "../../../db/data.json";
 
 export default function Day() {
-  const day = 1;
-  const wordList = dummy.words.filter((word) => word.day === day);
-  console.log(wordList);
+  // url 을 포함된 값을 얻을 때는 useParams 훅을 사용한다.
+  // const a = useParams();
+  // console.log(a); // {day: '1'} 이런식으로 들어옴
+
+  const day = useParams().day;
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
     <>
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (
