4086a953467d1236eec8a9f2b0f206b3b53e30cd
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-19 19:18:11 +0900

    update: Day.js 수정
    주석 추가: 코드에 대한 설명 추가

--- src/components/main/components/Day.js
+++ src/components/main/components/Day.js
@@ -6,7 +6,10 @@ export default function Day() {
   // const a = useParams();
   // console.log(a); // {day: '1'} 이런식으로 들어옴
 
+  // 현재 URL의 :day 부분의 값을 가져옴
   const day = useParams().day;
+
+  // useParams 로 가져온 day 는 기본적으로 문자열이기 때문에 숫자로 변환하여 word.day 와 비교할 수 있게 만듦
   const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
