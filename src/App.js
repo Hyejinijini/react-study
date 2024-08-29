2bf490b22d7fa9108d1cc74d41611193d0d27f7b
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-29 19:50:28 +0900

    update: App.js 라우터 경로 추가
    단어, day 추가하는 페이지 컴포넌트 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./assets/styles/app/App.css";
+
 import Header from "./components/header/Header";
 import Day from "./components/main/components/Day.js";
 import DayList from "./components/main/DayList.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmptyPage from "./components/main/components/EmptyPage.js";
+import CreateWord from "./components/main/components/CreateWord.js";
+import CreateDay from "./components/main/components/CreateDay.js";
 
 function App() {
   return (
@@ -16,6 +19,8 @@ function App() {
           예를 들어, day 1 로 들어갔을 때 day 라는 변수로 1 이라는 값을 얻을 수 있다.
           만약 :id 면 id 라는 변수로 1 의 값을 얻을 수 있다. */}
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           {/* 와일드카드 경로를 사용해서 잘못된 경로에 대응하는 라우트를 만들었음. */}
           <Route path="*" element={<EmptyPage />} />
         </Routes>
